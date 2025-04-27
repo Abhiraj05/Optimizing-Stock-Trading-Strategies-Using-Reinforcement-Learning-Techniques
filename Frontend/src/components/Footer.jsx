4184d21ffe7aac83977025d02bfcd06b1ad459ca
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-04-27 11:46:35 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
     <footer className="bg-gray-800 text-white">
       <div className="max-w-6xl mx-auto px-4 py-4 text-center">
-        <p>&copy; 2025 Stock. All rights reserved.</p>
+        <p>&copy;Trade Pro. All rights reserved.</p>
       </div>
     </footer>
   );
