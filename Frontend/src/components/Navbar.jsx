88e00cdb4418b1b0ee223e904f509d8d34612c72
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-04-27 11:04:05 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.png"
 export const Navbar = () => (
-    <nav className="bg-gray-800 text-white border-b-2 border-green-400">
+    <nav className="bg-gray-800 text-white border-b-[0.5px] border-green-400">
       <div className="max-w-6xl mx-auto ">
         <div className="flex justify-center items-center gap-48 lg:flex lg:justify-between lg:gap-x-96">
           <div>
