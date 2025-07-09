3be9a666ee313fb15506943bf70f5433a0978622
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-07-09 13:27:30 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,3 @@
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from './pages/Home';
-// import Chart from './pages/Chart';
-
-
-// function App(){
-//   return(
-//     <>
-//     <Router>
-//  <Routes><Route path="/" element={<Home />} />
-//     <Route path="/chart" element={<Chart />} />
-//  </Routes>
-//  </Router> 
-//     </>
-//   )
-// }
-
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from './components/AnimatedRoutes';
